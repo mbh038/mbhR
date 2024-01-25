50bba05b9da33fcb90b34a3b557b5d5376d7d049
mbh038 <mbh038@gmail.com>
2024-01-25 10:42:08 +0000

    power curve

---
+++ R/wind_power_curve.R
@@ -0,0 +1,12 @@
+#' Power curve for a generic wind turbine
+#'
+#'
+#' @format A tibble with 25 rows and 2 variables:
+#' \describe{
+#'   \item{`v`}{dbl wind speed in m/s}
+#'   \item{`P`}{dbl power output of wind turbine} 
+
+
+#' }
+#' @source Typical of many published power curves for large (>1 MW) wind turbines
+"wind_power_curve"
