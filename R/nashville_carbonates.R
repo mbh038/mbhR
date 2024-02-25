3e7b5297dfb1def750558f8af4f97690a8ccb7de
mbh038 <mbh038@gmail.com>
2024-02-25 10:00:53 +0000

    acknowledgements for nashville carbonates data set

--- R/nashville_carbonates.R
+++ R/nashville_carbonates.R
@@ -4,15 +4,16 @@
 #' \describe{
 #'   \item{sampleID}{sample ID}
 #'   \item{stratPosition}{depth beneath the surface (cm)}
-#'   \item{d13C}{C13 condentration}
-#'   \item{d18O}{O18 condentration}
+#'   \item{d13C}{C13 concentration}
+#'   \item{d18O}{O18 concentration}
 #'   \item{Al}{Aluminium concentration}
-#'   \item{Ca}{Aluminium concentration}
-#'   \item{Fe}{Aluminium concentration}
-#'   \item{Mg}{Aluminium concentration}
-#'   \item{Mn}{Aluminium concentration}
-#'   \item{Si}{Aluminium concentration}
+#'   \item{Ca}{Calcium concentration}
+#'   \item{Fe}{Iron concentration}
+#'   \item{Mg}{Magnesiumc oncentration}
+#'   \item{Mn}{Manganese concentration}
+#'   \item{Si}{Silicon concentration}
 #' }
 #' @source {From the GEOL 8370 site at http://stratigrafia.org/8370/data/}
+#' @source {Used by Steven Holland (2019) in his PCA tutorial: http://stratigrafia.org/8370/handouts/pcaTutorial.pdf}
 #' @source {Theiling, B.P. et al. (2007) ‘Heterogeneity in Geochemical Expression of Subaerial Exposure in Limestones, and Its Implications for Sampling to Detect Exposure Surfaces’, Journal of Sedimentary Research, 77(2), pp. 159–169.} \doi{10.6073/pasta/98b16d7d563f265cb52372c8ca99e60f}
 "nashville_carbonates"
